b4bcd195bc00bbd9d027fc5dcb43ee0ff73ece71
chanduparmar <chandu.parmar33@gmail.com>
2017-03-12 21:02:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('artical-one', function(req,res){
+app.get('/artical-one', function(req,res){
    res.send("Artical one");
 });
-app.get('artical-two', function(req,res){
+
+app.get('/artical-two', function(req,res){
    res.send("Artical two");
 });
-app.get('artical-three', function(req,res){
+
+app.get('/artical-three', function(req,res){
    res.send("Artical three");
 });
 
