0b46714deb0f211a70cb4078fa494d70f55e2326
chanduparmar <chandu.parmar33@gmail.com>
2017-03-13 08:32:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.get('/artical-two', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req,res){
+     counter = counter + 1;
+     res.send(counter.toString());
+});
+
 app.get('/artical-three', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
 });
