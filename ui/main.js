e5d6af6f581079ac923723586071da7933ac137e
chanduparmar <chandu.parmar33@gmail.com>
2017-03-13 09:05:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ submit.onclick = function(){
     var names = ['name1','name2','name3'];
     var list = '';
     for(var i=0;i<names.length;i++){
-        list += '<li>' + name1[i] + '</li>';
+        list == '<li>' + name1[i] + '</li>';
     }
     var ul = document.getElementById('name-list');
     ul.innterHTML = list;
